8ab801f612b4b6d5db12ed8d64a92cf369d37738
Ishamoridin <Ishamoridin@googlemail.com>
2022-11-25 13:12:49 +0000

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,76 @@
-import logo from './logo.svg';
+import { useState } from "react";
 import './App.css';
+import { evaluate, sqrt } from "mathjs";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+const App = () => {
+    const [displayValue, editDisplayValue] = useState(`0`)
+    const buttonArray =['(','+','-','*','÷',')',1,2,3,4,5,6,7,8,9,'C',0,'=',`√`]
+    const classHandler = (button) => {
+        switch(button){
+        case (1):{return (`third`)}
+        case (2):{return (`third`)}
+        case (3):{return (`third`)}
+        case (4):{return (`third`)}
+        case (5):{return (`third`)}
+        case (6):{return (`third`)}
+        case (7):{return (`third`)}
+        case (8):{return (`third`)}
+        case (9):{return (`third`)}
+        case (0):{return (`third`)}
+        case (`(`):{return (`sixth`)}
+        case (`)`):{return (`sixth`)}
+        case (`C`):{return (`third`)}
+        case (`=`):{return (`third`)}
+        case (`+`):{return (`sixth`)}
+        case (`-`):{return (`sixth`)}
+        case (`*`):{return (`sixth`)}
+        case (`÷`):{return (`sixth`)}
+        case ('√'):{return('full')}
+        default:{}
+        
+    }}
+    const clickHandler = (button) => {
+        switch (button){
+            case 1:{return(editDisplayValue(displayValue+`${button}`))}
+            case 2:{return(editDisplayValue(displayValue+`${button}`))}
+            case 3:{return(editDisplayValue(displayValue+`${button}`))}
+            case 4:{return(editDisplayValue(displayValue+`${button}`))}
+            case 5:{return(editDisplayValue(displayValue+`${button}`))}
+            case 6:{return(editDisplayValue(displayValue+`${button}`))}
+            case 7:{return(editDisplayValue(displayValue+`${button}`))}
+            case 8:{return(editDisplayValue(displayValue+`${button}`))}
+            case 9:{return(editDisplayValue(displayValue+`${button}`))}
+            case 0:{return(editDisplayValue(displayValue+`${button}`))}
+            case '+':{return(editDisplayValue(displayValue+`${button}`))}
+            case '-':{return(editDisplayValue(displayValue+`${button}`))}
+            case '*':{return(editDisplayValue(displayValue+`${button}`))}
+            case '÷':{return(editDisplayValue(displayValue+`/`))}
+            case '(':{return(editDisplayValue(displayValue+`${button}`))}
+            case ')':{return(editDisplayValue(displayValue+`${button}`))}
+            case 'C':{return(editDisplayValue(`0`))}
+            case '=':{return(editDisplayValue(evaluate(`${displayValue}`)))}
+            case '√':{return(editDisplayValue(sqrt(displayValue)))}
+            default: {return (console.log(`invalid button pressed`))}            
+        }
+    }
+    const Button = buttonArray.map((button) => {
+        return (
+        <button id={button} className={classHandler(button)} onClick={() => clickHandler(button)}>{button}</button>
+        )
+    })
+    return (
+        <div>
+
+            <div className='wrapper'>
+                
+                <div className="button-box">
+                    <h1>Most Excellent Calculator</h1>
+                    <input value={displayValue}/>
+                    {Button}
+                </div>
+            </div>
+
+        </div>);
 }
-
 export default App;
